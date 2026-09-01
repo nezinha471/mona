31c4c42eb287b5ad70405df48beb94436acbd735
mona <valentewilliams@icloud.com>
2026-09-01 21:25:31 +0100

    acrecentar o arquivo app-expo

---
+++ app-expo (2)/app-expo/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+import Homepage from './src/pages/Homepage' 
+  
+  const App = () => {
+
+   return (
+
+    <Homepage/>
+
+   );
+
+}
+
+export default App;
---
+++ app-expo (2)/app-expo/src/pages/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const Homepage = () => {  
+  
+  return (
+
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+    
+      <Text> Puc Minas </Text>
+
+     </View>
+
+   );
+
+}
+
+export default Homepage;
